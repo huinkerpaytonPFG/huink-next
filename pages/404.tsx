c9890d94bc2201453234c0ff912958746c96f5a0
Huinker, Payton <huinker.payton@principal.com>
2021-11-30 14:08:01 -0600

    small text updates

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,12 +9,12 @@ const Error404 = () => {
 
 	return (
 		<>
-			<h1 className="pds-typography-center">Oops. This is not the page you were looking for...</h1>
+			<h1 className="pds-typography-center">Ope. This is not the page you were looking for...</h1>
 			<Row className="pds-util-margin-top-20" justifyContent="center">
 				<Col lg={6}>
 					<Card>
 						{/* <Image className={styles["jpg-404"]} layout="responsive" src={jpg404} alt="Not the droid you are looking for" /> */}
-						<Image className={styles["gif-404"]} layout="responsive" src={gif404}alt="Not the droid you are looking for" />
+						<Image className={styles["gif-404"]} layout="responsive" src={gif404}alt="Not the droid you are looking for" priority />
 					</Card>
 				</Col>
 			</Row>
