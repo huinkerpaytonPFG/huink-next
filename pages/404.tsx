d9fdba9f2013a220d5e0de0f50a1cc54b9336c75
Huinker, Payton <huinker.payton@principal.com>
2021-11-30 11:21:24 -0600

    drastically improved the 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,11 +5,12 @@ const Error404 = () => {
 
 	return (
 		<>
-			<h1 className="pds-typography-center">Oops. We couldn&#39;t find that Pok&eacute;mon!</h1>
+			<h1 className="pds-typography-center">Oops. This is not the page you were looking for...</h1>
 			<Row className="pds-util-margin-top-20" justifyContent="center">
 				<Col lg={6}>
 					<Card>
-						<CardImage style={{ height: "auto", margin: "auto" }} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/201.png" alt={`Image of Unown, the 201st Pokemon, from the front profile`} />
+						<source srcSet="https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/05/ANH-Ben-identification.jpg" media="(prefers-reduced-motion: reduce)"></source> 
+  					<img srcSet="https://media4.giphy.com/media/4560Nv2656Gv0Lvp9F/giphy.gif" alt="Not the droid you are looking for"/>
 					</Card>
 				</Col>
 			</Row>
