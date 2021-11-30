b2dffc01050b3294d90b7c9d4521c5250eb9c0b3
Huinker, Payton <huinker.payton@principal.com>
2021-11-30 12:09:17 -0600

    Using the Image component
    Optimizing, but it adds all kinds of styling that I dont really care for.. Will need to figure out for accessibility

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,9 @@
 import { Row, Col } from "@pds-react/grid";
-import Card, { CardImage } from "@pds-react/card";
+import Card from "@pds-react/card";
+import jpg404 from "../public/images/404.jpg"
+import gif404 from "../public/images/404.gif"
+import Image from 'next/image'
+import styles from "../styles/404.module.css"
 
 const Error404 = () => {
 
@@ -9,8 +13,8 @@ const Error404 = () => {
 			<Row className="pds-util-margin-top-20" justifyContent="center">
 				<Col lg={6}>
 					<Card>
-						<source srcSet="https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/05/ANH-Ben-identification.jpg" media="(prefers-reduced-motion: reduce)"></source> 
-  					<img srcSet="https://media4.giphy.com/media/4560Nv2656Gv0Lvp9F/giphy.gif" alt="Not the droid you are looking for"/>
+						{/* <Image className={styles["jpg-404"]} layout="responsive" src={jpg404} alt="Not the droid you are looking for" /> */}
+						<Image className={styles["gif-404"]} layout="responsive" src={gif404}alt="Not the droid you are looking for" />
 					</Card>
 				</Col>
 			</Row>
