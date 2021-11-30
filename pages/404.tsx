4e9b851dcad690d2575b5107de32c588311a19e9
Huinker, Payton <huinker.payton@principal.com>
2021-11-30 11:10:33 -0600

    typo

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ const Error404 = () => {
 	return (
 		<>
 			<h1 className="pds-typography-center">Oops. We couldn&#39;t find that Pok&eacute;mon!</h1>
-			<Row className="pds-util-margin-top-20" justiftContent="center">
+			<Row className="pds-util-margin-top-20" justifyContent="center">
 				<Col lg={6}>
 					<Card>
 						<CardImage style={{ height: "auto", margin: "auto" }} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/201.png" alt={`Image of Unown, the 201st Pokemon, from the front profile`} />
