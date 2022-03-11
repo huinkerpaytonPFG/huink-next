f3bc195b78fa9524cefbcbc1c2265c402429ab67
Huinker, Payton <huinker.payton@principal.com>
2022-03-11 09:07:44 -0600

    Bummer no cypress utils.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,1 @@
-const cypressUtilsPlugin = require("@ris/cypress-utils/src/plugin");
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-	return cypressUtilsPlugin(on, config, {
-		prodBaseUrl: "https://www.huink.dev",
-	});
-};
